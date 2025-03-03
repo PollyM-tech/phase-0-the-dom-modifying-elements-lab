9cac54f0582b23250df27aff400c05a75381ff14
Pauline Moraa <pmoraa431@gmail.com>
2025-03-03 23:45:16 +0300

    Creating and Inserting DOM Nodes

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const mainElement = document.querySelector('main#main');
+
+// Removing the element 
+if (mainElement) {
+  mainElement.remove();
+}
+//create an <h1> element
+const newHeader = document.createElement("h1");
+//newHeader variable that points to the <h1> node has an id of 'victory'
+newHeader.id = "victory";
+//the 'newHeader' variable that points to the <h1> node with an id of 
+// 'victory' has the text "YOUR-NAME is the champion"
+newHeader.textContent = "Pauline is the champion"; 
